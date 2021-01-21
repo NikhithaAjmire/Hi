f9ac3e658f7cc1bab1bedd480322eff6b904f108
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 11:54:13 +0530

    ADDED:Hello

--- src/Hii/Hi.java
+++ src/Hii/Hi.java
@@ -5,6 +5,7 @@ public class Hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("-----------------------Hello everyone--------------------------");
+		System.out.println("-----------------------Hi--------------------------");
 		
 	}
 
